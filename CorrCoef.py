82cf8c428593c29eb957371fde522e05071f4d32
Eric <mcand3r@gmail.com>
2024-04-23 00:32:37 -0400

    merged several tables together National Incident-Based Reporting System (NIBRS) Tables for crimes based on location

--- CorrCoef.py
+++ CorrCoef.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_excel("Crimes_Against_Society_Offenses_Offense_Category_by_Location_2022.xlsx", sheet_name="Table 19 NIBRS 2022")
+data = pd.read_excel("bigdata.xlsx", sheet_name="Table 19 NIBRS 2022")
 
 Matrix = data.corr()
 
